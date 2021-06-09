7d55f85c3a8a5c1fbe7c25e1ab84b2c5d6a358d4
Catanio <mauricatanio@yahoo.com.br>
2021-06-09 17:15:25 -0300

    Add System Factory

---
+++ DataStructures/SystemFactory.cs
@@ -0,0 +1,20 @@
+﻿using DataStructures;
+using DataStructures.Definitions;
+using System;
+
+namespace Multithread_ETL
+{
+    class SystemFactory
+    {
+        public static ISystem Create(SystemTypes systemType)
+        {
+            return systemType switch
+            {
+                SystemTypes.System_A => throw new NotImplementedException(),
+                SystemTypes.System_B => throw new NotImplementedException(),
+                SystemTypes.System_C => throw new NotImplementedException(),
+                _ => throw new ArgumentOutOfRangeException($"The system {systemType} doesn't exists")
+            };
+        }
+    }
+}
