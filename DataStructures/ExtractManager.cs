fc697fc4e51a71d775302ba145757992a086641e
Catanio <mauricatanio@yahoo.com.br>
2021-06-11 11:48:37 -0300

    Add Extraction Manager

---
+++ DataStructures/ExtractManager.cs
@@ -0,0 +1,20 @@
+﻿using DataStructures.Definitions;
+using Multithread_ETL;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class ExtractManager
+    {
+        internal readonly ISystem system;
+
+        public ExtractManager(Definitions.SystemTypes systemType)
+        {
+            system = SystemFactory.Create(systemType);
+        }
+
+        public void BeginExtraction(List<Tables> tables) => system.RunExtractionAsync(tables);
+
+
+    }
+}
